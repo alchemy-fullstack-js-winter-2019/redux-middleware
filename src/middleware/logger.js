bdd0b065d49f690e403cc8da680337d98cb33d08
Kate Dameron <katerj33@gmail.com>
2019-03-05 13:45:36 -0800

    travis added

--- src/middleware/logger.js
+++ src/middleware/logger.js
@@ -1,11 +1,12 @@
 // store to next middleware to action dispatch
 // always call next on the action
+// all actions go through the middleware
 
 export const logger = store => next => action => {
   console.log('action type & state', action.type, action.payload);
 
   const prevState = store.getState();
-  next(action);
+  next(action); /*passes the action to the reducer to update state*/
   const nextState = store.getState();
 
   if(prevState !== nextState) {
