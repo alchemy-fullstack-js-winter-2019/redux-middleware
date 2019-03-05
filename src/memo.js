679a16d2d43bbad45b031844cc72c7729d051de4
Kate Dameron <katerj33@gmail.com>
2019-03-05 11:56:07 -0800

    green test reactPropsEquality

--- src/memo.js
+++ src/memo.js
@@ -1,6 +1,14 @@
 export function defaultEquality(prevArgs, nextArgs) {
   return prevArgs.length === nextArgs.length &&
-  prevArgs.every((arg, i)=> {
+  prevArgs.every((arg, i) => {
     return arg === nextArgs[i];
+  })
+}
+
+export function reactPropsEquality(props, prevProps) {
+  const newProps = Object.keys(props);
+  const oldProps = Object.keys(prevProps);
+  return newProps.length === oldProps.length && oldProps.every((prop, i) => {
+    return prop === newProps[i];
   })
 }
--- src/memo.test.js
+++ src/memo.test.js
@@ -1,24 +1,43 @@
-import { defaultEquality } from './memo';
+import { defaultEquality, reactPropsEquality } from './memo';
 
 describe('memo', () => {
-  it('checks for equality - returns true', () => {
-    const prevArgs = [1, 2, 3];
-    const nextArgs = [1, 2, 3];
+  describe('defaultEquality', () => {
+    it('checks for equality - returns true', () => {
+      const prevArgs = [1, 2, 3];
+      const nextArgs = [1, 2, 3];
     
-    expect(defaultEquality(prevArgs, nextArgs)).toBeTruthy();
+      expect(defaultEquality(prevArgs, nextArgs)).toBeTruthy();
+    });
+
+    it('checks for inequality - returns false', () => {
+      const prevArgs = [1, 2, 3];
+      const nextArgs = [1, 3, 2];
+    
+      expect(defaultEquality(prevArgs, nextArgs)).toBeFalsy();
+    });
+
+    it('can check for extra args', () => {
+      const prevArgs = [1, 2, 3];
+      const nextArgs = [1, 2, 3, 4];
+    
+      expect(defaultEquality(prevArgs, nextArgs)).toBeFalsy();
+    });
   });
 
-  it('checks for inequality - returns false', () => {
-    const prevArgs = [1, 2, 3];
-    const nextArgs = [1, 3, 2];
+  describe('reactPropsEquality', () => {
+    it('checks for prop equality', () => {
+      const prevProps = {
+        a: 1,
+        b: 2,
+        c: 3
+      };
+      const newProps = {
+        a: 1,
+        b: 2,
+        c: 3
+      };
     
-    expect(defaultEquality(prevArgs, nextArgs)).toBeFalsy();
-  });
-
-  it('can check for extra args', () => {
-    const prevArgs = [1, 2, 3];
-    const nextArgs = [1, 2, 3, 4];
-    
-    expect(defaultEquality(prevArgs, nextArgs)).toBeFalsy();
+      expect(reactPropsEquality(prevProps, newProps)).toBeTruthy();
+    });
   });
 });
