a261ceb7bdeccd75ef59d29332ae21f0499e0db4
Carmen V Ramos <carmenvramos@yahoo.com>
2019-03-05 14:28:49 -0800

    Thunk middleware & test passing

--- src/middleware/thunk.js
+++ src/middleware/thunk.js
@@ -1,3 +1,8 @@
 export const thunk = store => next => action => {
-  
-}
+
+  if(typeof action === 'function') {
+    return action(store.dispatch, store.getState);
+  } else {
+    next(action);
+  }  
+};
