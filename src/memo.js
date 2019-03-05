e0d4b3f9a461b093841b9b109e45f721457d315d
Kate Dameron <katerj33@gmail.com>
2019-03-05 11:47:45 -0800

    green test for defaultEquality checking for args that do not match and green test checks for length

--- src/memo.js
+++ src/memo.js
@@ -1,5 +1,6 @@
 export function defaultEquality(prevArgs, nextArgs) {
-  return prevArgs.every((arg, i)=> {
+  return prevArgs.length === nextArgs.length &&
+  prevArgs.every((arg, i)=> {
     return arg === nextArgs[i];
   })
 }
--- src/memo.test.js
+++ src/memo.test.js
@@ -7,4 +7,18 @@ describe('memo', () => {
     
     expect(defaultEquality(prevArgs, nextArgs)).toBeTruthy();
   });
+
+  it('checks for inequality - returns false', () => {
+    const prevArgs = [1, 2, 3];
+    const nextArgs = [1, 3, 2];
+    
+    expect(defaultEquality(prevArgs, nextArgs)).toBeFalsy();
+  });
+
+  it('can check for extra args', () => {
+    const prevArgs = [1, 2, 3];
+    const nextArgs = [1, 2, 3, 4];
+    
+    expect(defaultEquality(prevArgs, nextArgs)).toBeFalsy();
+  });
 });
