183eca3e6018a1c167c219196a856fb8b7c2414d
Kristin Hortsch <kristinhortsch@gmail.com>
2019-03-05 13:43:38 -0800

    passing middleware action logger test

--- src/memo.test.js
+++ src/memo.test.js
@@ -2,23 +2,23 @@ import { defaultEquality, memoize } from './memo';
 
 describe('default equality', () => {
   it('can tell if arguments are the same', () => {
-    const prevArguments=[1, 2, 3];
-    const newArguments=[1, 2, 3];
+    const prevArguments = [1, 2, 3];
+    const newArguments = [1, 2, 3];
     expect(defaultEquality(prevArguments, newArguments)).toEqual(true);
-  })
+  });
 
   it('can tell if arguments are not the same', () => {
-    const prevArguments=[1, 2, 4];
-    const newArguments=[1, 2, 3];
+    const prevArguments = [1, 2, 4];
+    const newArguments = [1, 2, 3];
     expect(defaultEquality(prevArguments, newArguments)).toEqual(false);
-  })
+  });
 
   it('can tell if arguments are different because of length', () => {
-    const prevArguments=[1, 2, 3];
-    const newArguments=[1, 2, 3, 4];
+    const prevArguments = [1, 2, 3];
+    const newArguments = [1, 2, 3, 4];
     expect(defaultEquality(prevArguments, newArguments)).toEqual(false);
-  })
-})
+  });
+});
 
 describe('memoize function', () => {
   it('memoizes a function', () => {
