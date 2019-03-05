726b96383e26c6547e75a3c73691911bd4102ecc
Tyler Corbett <thetylercorbett@gmail.com>
2019-03-05 14:28:55 -0800

    thunk middleware test passing

---
+++ src/middleware/logger.js
@@ -0,0 +1,11 @@
+export const logger = store => next => action => {
+  console.log('INCOMING ACTION', action);
+  const prevState = store.getState();
+  next(action);
+  const nextState = store.getState();
+
+  if(prevState !== nextState) {
+    console.log('STATE UPDATED', nextState);
+  }
+};
+
