a111e8de3845ac61d24a428d6209e37f3e3bed0a
Lance Owen Merrill <lanceomerrill@gmail.com>
2019-03-06 08:56:04 -0800

    fixing travis errors

--- memo.js
+++ memo.js
@@ -1,19 +1,19 @@
 export const areEqual = (prevArg, nextArg) => {
     return prevArg.length === nextArg.length &&
         prevArg.every((arg, i)  => {
-        return arg === nextArg[i]
-    });
+            return arg === nextArg[i];
+        });
 };
 
 export const memoize = (fn, equal = areEqual) => {
     let prevArg = null;
-    let result = null
+    let result = null;
     return function(...args){
         if(prevArg && equal(prevArg, args)){
             return result;
         }
         prevArg = args;
         result = fn(...args);
-        return result
+        return result;
     };
 };
