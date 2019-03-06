48f77645fdc4b47e1bb258fda7efb83245b022e2
Carmen V Ramos <carmenvramos@yahoo.com>
2019-03-05 16:17:44 -0800

    Updated reducers/ron file by importing FETCH_FACTS action

---
+++ src/containers/AllFacts.js
@@ -0,0 +1,9 @@
+import React, { PureComponent} from 'react';
+
+
+export default class AllFacts extends PureComponent {
+  state = {
+    
+  }
+}
+
---
+++ src/facts/Facts.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function  Facts({ facts }) {
+  console.log('facts: ', facts);
+  const factList = facts.map((fact, i) => 
+    <li key={i}>{fact}</li>);
+
+  return (
+    <ul> 
+      {factList}
+    </ul>
+  );
+}
+
+Facts.propTypes = {
+  facts: PropTypes.array.isRequired
+};
+
+export default Facts;
