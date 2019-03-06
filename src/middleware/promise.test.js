5dce91f72c82c8513cd60267f87ad3638e360d84
Kate Dameron <katerj33@gmail.com>
2019-03-06 09:52:27 -0800

    promise middleware started with isPromise and 2 green tests

---
+++ src/middleware/promise.test.js
@@ -0,0 +1,14 @@
+import { isPromise } from './promise';
+
+describe('promise middleware', () => {
+  it('returns false if not passed a promise', () => {
+    const result = isPromise('abc');
+    expect(result).toBeFalsy();
+  });
+
+  it('returns true if passed a promise', () => {
+    const promise = Promise.resolve(1234);
+    const result = isPromise(promise);
+    expect(result).toBeTruthy();
+  });
+});
