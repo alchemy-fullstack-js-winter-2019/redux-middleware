0a519f941e4712c9cb68e39682fc926958495ab2
Kristin Hortsch <kristinhortsch@gmail.com>
2019-03-06 10:24:13 -0800

    passing promiseMiddleware test

--- src/middleware/promise.js
+++ src/middleware/promise.js
@@ -2,4 +2,25 @@ export const isPromise = payload => {
   return Promise.resolve(payload) === payload;
 };
 
+export const promiseMiddleware = store => next => action => {
+  if(!isPromise(action.payload)) {
+    return next(action);
+  }
+  store.dispatch({ type: 'LOAD_START' });
+
+  action.payload.then(result => {
+    store.dispatch({ type: 'LOAD_END' });
+    store.dispatch({
+      type: action.type,
+      payload: result
+    });
+  })
+    .catch(error => {
+      store.dispatch({
+        type: 'PROMISE_ERROR',
+        payload: error
+      });
+    });
+};
+  
 
--- src/middleware/promise.test.js
+++ src/middleware/promise.test.js
@@ -1,14 +1,55 @@
-import { isPromise } from './promise';
- 
+import { isPromise, promiseMiddleware } from './promise';
+import { applyMiddleware } from 'redux';
+import { createStore } from 'redux';
+
 describe('promise middleware', () => {
-  it('returns false if not passed a promise', () => {
-    const result = isPromise(1234);
-    expect(result).toBeFalsy();
+  describe('isPromise', () => {
+    it('returns false if not passed a promise', () => {
+      const result = isPromise(1234);
+      expect(result).toBeFalsy();
+    });
+  
+    it('returns true if passed a promise', () => {
+      const promise = Promise.resolve(1234);
+      const result = isPromise(promise);
+      expect(result).toBeTruthy();
+    });
   });
 
-  it('returns true if passed a promise', () => {
-    const promise = Promise.resolve(1234);
-    const result = isPromise(promise);
-    expect(result).toBeTruthy();
+  describe('promiseMiddleware', () => {
+    let reducer = null;
+    let store = null;
+
+    beforeEach(() => {
+      reducer = jest.fn();
+      store = createStore(
+        reducer,
+        applyMiddleware(promiseMiddleware)
+      );
+    });
+
+    it('dispatches all actions on promise resolve', () => {
+      const promise = Promise.resolve(123);
+      const action = {
+        type: 'MY_ACTION',
+        payload: promise
+      };
+
+      store.dispatch(action);
+
+      return promise.then(() => {
+        expect(reducer).toHaveBeenCalledWith(undefined, {
+          type: 'LOAD_START'
+        });
+        expect(reducer).toHaveBeenCalledWith(undefined, {
+          type: 'LOAD_END'
+        });
+        expect(reducer).toHaveBeenCalledWith(undefined, {
+          type: 'MY_ACTION',
+          payload: 123
+        });
+
+      });
+    });
   });
 });
