3745982e633c138100fb04d7baef925e99d5a693
Sophie Graham <sophiergraham1@gmail.com>
2019-03-06 12:41:36 -0800

    added final pieces to memo files

--- src/components/memo/memo.js
+++ src/components/memo/memo.js
@@ -1,10 +1,22 @@
-// export function defaultEquality(prevArguments, nextArgument){
-//   return prevArguments.length === nextArgument.length &&
-//     prevArguments.every((arg, i) => {
-//       return arg === nextArguments[i];
-//     });
-// }
+export function defaultEquality(prevArguments, nextArguments){
+  return prevArguments.length === nextArguments.length &&
+    prevArguments.every((arg, i) => {
+      return arg === nextArguments[i];
+    });
+}
 
-// export function memoize(fn, equalityFn = defaultEquality) {
-  
-// }
+export function memoize(fn, equalityFn = defaultEquality) {
+  let lastArguments = null;
+  let lastResult = null;
+
+  return function(...currentArguments) {
+    if(lastArguments && equalityFn(lastArguments, currentArguments)) {
+      return lastResult;
+    }
+
+    lastArguments = currentArguments;
+    lastResult = fn(...currentArguments);
+
+    return lastResult;
+  };
+}
