eb6bc3c7f4a77a7fbc9adf66d0ad1ce3cab86303
TEONNA ZARAGOZA <teonna.zaragoza@gmail.com>
2019-03-06 13:08:56 -0800

    added a button

--- src/reducers/ron.js
+++ src/reducers/ron.js
@@ -1,11 +1,17 @@
 const initialState = {
-  facts: []
+  facts: [],
+  loading: false
 };
 
 export default function reducer(state = initialState, action) {
   switch(action.type) {
     case 'FETCH_FACTS':
       return { ...state, facts: action.payload };
+    case 'UPDATE_LOADING':
+      return {
+        ...state,
+        loading: action.payload
+      };
     default:
       return state;
   }
