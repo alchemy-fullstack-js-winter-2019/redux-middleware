917fabbe4a680f4d51425c3182e9a83ccc96ae23
Shaba9 <shaba92@gmail.com>
2019-03-06 10:26:40 -0800

    promiseMiddleware and tests

--- src/middleware/promise.js
+++ src/middleware/promise.js
@@ -1,1 +1,19 @@
-export const isPromise = payload => typeof payload.then === 'function';
+export const isPromise = payload => payload && typeof payload.then === 'function';
+
+export const promiseMiddleware = store => next => action => {
+  if(!isPromise(action.payload)) return next(action);
+
+  store.dispatch({ type: 'LOAD_START' });
+  action.payload
+    .then(res => {
+      store.dispatch({ type: 'LOAD_END' });
+      store.dispatch({ 
+        type: action.type,
+        payload: res
+      });
+    })
+    .catch(err => store.dispatch({
+      type: 'PROMISE_ERROR',
+      payload: err
+    }));
+};
--- src/middleware/promise.test.js
+++ src/middleware/promise.test.js
@@ -1,4 +1,5 @@
-import { isPromise } from './promise';
+import { isPromise, promiseMiddleware } from './promise';
+import { applyMiddleware, createStore } from 'redux';
 
 describe('promise middleware', () => {
   it('returns false if not passed a promise', () => {
@@ -9,5 +10,43 @@ describe('promise middleware', () => {
   it('returns true if passed a promise', () => {
     const result = isPromise(Promise.resolve(123));
     expect(result).toBeTruthy();
+  });
+});
+
+describe('middleware', () => {
+  let reducer = null;
+  let store = null;
+
+  beforeEach(() => {
+    reducer = jest.fn();
+    store = createStore(
+      reducer,
+      applyMiddleware(promiseMiddleware)
+    );
+  });
+
+  it('dispatches all actions on promise resolve', () => {
+    const promise = Promise.resolve(123);
+    const action = {
+      type: 'TEST_ACTION',
+      payload: promise
+    };
+
+    store.dispatch(action);
+
+    promise.then(() => {
+      expect(reducer).toHaveBeenCalledWith(undefined, {
+        type: 'LOAD_START'
+      });
+
+      expect(reducer).toHaveBeenCalledWith(undefined, {
+        type: 'LOAD_END'
+      });
+
+      expect(reducer).toHaveBeenCalledWith(undefined, {
+        type: 'TEST_ACTION',
+        payload: 123
+      });
+    });
   });
 });
