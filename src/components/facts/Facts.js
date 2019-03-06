9f2e98935a184c76ab02d7b1fb47b04d1ce16d10
Kate Dameron <katerj33@gmail.com>
2019-03-05 16:03:33 -0800

    Facts display component

---
+++ src/components/facts/Facts.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export default function Facts({ facts, fetch }) {
+  const factsList = facts.map((fact, i) => {
+    return <li onChange={fetch} key={i}>{fact}</li>;
+  });
+  return (
+    <ul>
+      {factsList}
+    </ul>
+  );
+}
+
+Facts.propTypes = {
+  facts: PropTypes.array.isRequired,
+  fetch: PropTypes.func.isRequired
+};
