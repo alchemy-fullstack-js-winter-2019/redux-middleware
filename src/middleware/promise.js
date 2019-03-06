e17814ef7bfa09f1e85bb23eb16c708124b281aa
Kristin Hortsch <kristinhortsch@gmail.com>
2019-03-06 10:38:07 -0800

    finished promise middleware stuff

--- src/middleware/promise.js
+++ src/middleware/promise.js
@@ -2,22 +2,33 @@ export const isPromise = payload => {
   return Promise.resolve(payload) === payload;
 };
 
-export const promiseMiddleware = store => next => action => {
+export const LOAD_START = 'LOAD_START';
+export const LOAD_END = 'LOAD_END';
+export const PROMISE_ERROR = 'PROMISE_ERROR';
+
+export const promiseMiddleware = ({ dispatch }) => next => (action) => {
+  const {
+    type,
+    loadStart = LOAD_START,
+    loadEnd = LOAD_END,
+    errorType = PROMISE_ERROR
+  } = action;
+
   if(!isPromise(action.payload)) {
     return next(action);
   }
-  store.dispatch({ type: 'LOAD_START' });
+  dispatch({ type: loadStart });
 
   action.payload.then(result => {
-    store.dispatch({ type: 'LOAD_END' });
-    store.dispatch({
-      type: action.type,
+    dispatch({
+      type,
       payload: result
     });
+    dispatch({ type: loadEnd });
   })
     .catch(error => {
-      store.dispatch({
-        type: 'PROMISE_ERROR',
+      dispatch({
+        type: errorType,
         payload: error
       });
     });
