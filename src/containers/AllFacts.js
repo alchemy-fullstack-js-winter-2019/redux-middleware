7350e7dcc138c20c9b15863967d923467788b276
pickuppatterns <pickup.patterns@gmail.com>
2019-03-07 07:49:20 -0800

    refactors container

--- src/containers/AllFacts.js
+++ src/containers/AllFacts.js
@@ -1,26 +1,12 @@
-import React, { PureComponent } from 'react';
-import PropTypes from 'prop-types';
 import Facts from '../components/facts/Facts';
 import { connect } from 'react-redux';
-import { getFacts } from '../selectors/ron';
+import { getFacts, isLoading } from '../selectors/ron';
 import { fetchFacts } from '../actions/fetchFacts';
 
 
-class AllFacts extends PureComponent {
-  static propTypes = {
-    facts: PropTypes.array.isRequired,
-    fetch: PropTypes.func.isRequired
-
-  };
-  componentDidMount() {
-    this.props.fetch();
-  }
-  render() {
-    return <Facts facts={this.props.facts} />;
-  }
-}
 const mapStateToProps = state => ({
-  facts: getFacts(state)
+  facts: getFacts(state),
+  isLoading: isLoading(state)
 });
 
 const mapDispatchToProps = dispatch => ({
@@ -28,6 +14,7 @@ const mapDispatchToProps = dispatch => ({
     dispatch(fetchFacts(10));
   }
 });
+const AllFacts = Facts;
 
 export default connect(
   mapStateToProps,
