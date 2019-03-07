86b6b254c41fe3e17704474ec5c418bdcf7f0917
pickuppatterns <pickup.patterns@gmail.com>
2019-03-07 06:57:28 -0800

    refactor destructured actions

--- src/middleware/promise.js
+++ src/middleware/promise.js
@@ -6,30 +6,28 @@ export const LOAD_START = 'LOAD_START';
 export const LOAD_END = 'LOAD_END';
 export const PROMISE_ERROR = 'PROMISE_ERROR';
 
+export const promiseMiddleware = ({ dispatch }) => next => action => {      const {
+  type,
+  loadStart = LOAD_START,
+  loadEnd = LOAD_END,
+  errorType = PROMISE_ERROR 
+} = action;
 
-
-export const promiseMiddleware = ({ dispatch }) => next => action => {
-  const {
-    type,
-    loadStart = 'LOAD_START',
-    loadEnd = 'LOAD_END',
-    errortype = 'PROMISE_ERROR'
-  } = action;
-}
 if(!isPromise(action.payload)) {
   return next(action);
 }
-dispatch({ type: 'LOAD_START' });
+dispatch({ type: LOAD_START });
 action.payload.then(payload => {
-  dispatch({ type: 'LOAD_END' });
+  dispatch({ type: LOAD_END });
   dispatch({
     type: action.type,
-    payload:result
+    payload
   });
 })
   .catch(err => {
     dispatch({
-      type: 'PROMISE_ERROR',
+      type: PROMISE_ERROR,
       payload: err
     });
   });
+};
