2a441de128ad214ccdd3c07795cecd6cbe570690
pickuppatterns <pickup.patterns@gmail.com>
2019-03-07 07:56:03 -0800

    updates app to use AllFacts

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,8 +1,6 @@
 import React from 'react';
-import store from '../../store';
-import { fetchFacts } from '../../actions/ron';
+import AllFacts from '../../containers/AllFacts';
 
 export default function App() {
-  store.dispatch(fetchFacts(10));
-  return <h1>THE FACTS OF RON SWANSON</h1>;
+  return <AllFacts />;
 }
