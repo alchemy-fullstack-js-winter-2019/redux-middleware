57e7d957536d38becff1540802064b3830d3173a
Sophie Graham <sophiergraham1@gmail.com>
2019-03-20 00:46:00 -0700

    promiseMiddleware.md, reactMemo.md done

--- src/components/memo/memo.test.js
+++ src/components/memo/memo.test.js
@@ -5,21 +5,18 @@ describe('Memo', () => {
     it('can tell if arguments are the same', () => {
       const prevArguments = [1, 2, 3];
       const nextArguments = [1, 2, 3];
-
       expect(defaultEquality(prevArguments, nextArguments)).toBeTruthy();
     });
 
     it('can tell if arguments are not the same', () => {
       const prevArguments = [1, 2, 3];
       const nextArguments = [1, 2, 4];
-
       expect(defaultEquality(prevArguments, nextArguments)).toBeFalsy();
     });
 
     it('can tell if arguments are different because of length', () => {
       const prevArguments = [1, 2, 3];
       const nextArguments = [1, 2, 3, 4];
-
       expect(defaultEquality(prevArguments, nextArguments)).toBeFalsy();
     });
   });
@@ -28,22 +25,16 @@ describe('Memo', () => {
     it('memoizes a function', () => {
       const fn = jest.fn();
       const memo = memoize(fn);
-
       memo(1, 2, 3);
       memo(1, 2, 3);
       memo(1, 2, 3);
       memo(1, 2, 3);
-
       expect(fn).toBeCalledTimes(1);
-
       memo(1, 2, 4);
       memo(1, 2, 4);
-
       expect(fn).toBeCalledTimes(2);
-
       memo(1, 2, 3);
       memo(1, 2, 4);
-
       expect(fn).toBeCalledTimes(4);
     });
   });
